f62e315d9a75f3a2730c29054fbb224eb529a34c
Ben Rybinski <45921913+benrybin@users.noreply.github.com>
2020-03-07 09:21:21 -0500

    and it begins

---
+++ src/main/java/com/lfg/tarkovLFG/TarkovLfgApplication.java
@@ -0,0 +1,13 @@
+package com.lfg.tarkovLFG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarkovLfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarkovLfgApplication.class, args);
+	}
+
+}
